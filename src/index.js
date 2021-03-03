c7a725cdb9d5ca8d227a659e3603f67cd97a7e02
Vyacheslav-Fialko <vyacheslav1@ukr.net>
2021-03-03 22:44:53 +0200

    feat: add breckets code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const open = new Set(bracketsConfig.map(pair => pair[0]));
+  const close = new Set(bracketsConfig.map(pair => pair[1]));
+  const relevant = bracketsConfig.reduce((acc, [open, close]) => ({ ...acc, [close]: open }), {});
+
+  let stack = [];
+  for (let char of str) {
+    if (open.has(char)) {
+      stack.push(char);
+    }
+    if (close.has(char)) {
+      if (relevant[char] !== stack.pop()) return false;
+    }
+  }
+
+  return stack.length === 0;
 }
